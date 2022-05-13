c23f62494da4508854281e55349b8e22fe2cb151
Nate Wolfe <wolfe.nate26@gmail.com>
2022-05-12 19:46:20 -0600

    initial folders

---
+++ Assets/script.js
@@ -0,0 +1,10 @@
+// making a quiz about JavaScirpt fundamentals
+
+//make a start button , then a timer starts showing the first question
+//start button is going to be event listener. timer is a set interval.
+//questions will be in an array
+
+//on a question is answered time is deducted from the timer, a penalty
+
+//if all questions are answered or the timer reaches 0 the game is over.
+//when the game is over an alert will pop up and save my Initials and score, local storage.
